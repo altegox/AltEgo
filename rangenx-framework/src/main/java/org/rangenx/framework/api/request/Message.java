554daca61331ade12864b3ce067b60e8f71dc132
citmina <gemingjia0201@163.com>
2025-02-20 00:03:00 +0800

    feat: add more construct method

--- rangenx-framework/src/main/java/org/rangenx/framework/api/request/Message.java
+++ rangenx-framework/src/main/java/org/rangenx/framework/api/request/Message.java
@@ -3,7 +3,7 @@ package org.rangenx.framework.api.request;
 public class Message {
 
     private String role;
-    
+
     private String content;
 
     public Message() {
@@ -12,6 +12,22 @@ public class Message {
     public Message(String role, String content) {
         this.role = role;
         this.content = content;
+    }
+
+    public static Message of(String role, String content) {
+        return new Message(role, content);
+    }
+
+    public static Message system(String content) {
+        return new Message("system", content);
+    }
+
+    public static Message assistant(String content) {
+        return new Message("assistant", content);
+    }
+
+    public static Message user(String content) {
+        return new Message("user", content);
     }
 
     public String getRole() {
