15c6fa6db9d1d90c1eac558383d0bf04aa480f21
citmina <gemingjia0201@163.com>
2025-02-21 02:09:01 +0800

    feat: add stream flag to LangModel

--- rangenx-framework/src/main/java/org/rangenx/framework/api/LangModel.java
+++ rangenx-framework/src/main/java/org/rangenx/framework/api/LangModel.java
@@ -5,11 +5,20 @@ public class LangModel {
     private String baseUrl;
     private String apiKey;
     private String modelName;
+    private boolean stream;
 
     private LangModel(Builder builder) {
         this.baseUrl = builder.baseUrl;
         this.apiKey = builder.apiKey;
         this.modelName = builder.modelName;
+        this.stream = builder.stream;
+    }
+
+    public LangModel(String baseUrl, String apiKey, String modelName, boolean stream) {
+        this.baseUrl = baseUrl;
+        this.apiKey = apiKey;
+        this.modelName = modelName;
+        this.stream = stream;
     }
 
     public LangModel() {
@@ -39,12 +48,21 @@ public class LangModel {
         this.modelName = modelName;
     }
 
+    public boolean isStream() {
+        return stream;
+    }
+
+    public void setStream(boolean stream) {
+        this.stream = stream;
+    }
+
     @Override
     public String toString() {
         return "LangModel {" +
                 "baseUrl='" + baseUrl + '\'' +
                 ", apiKey='" + apiKey + '\'' +
                 ", modelName='" + modelName + '\'' +
+                ", stream=" + stream +
                 '}';
     }
 
@@ -53,6 +71,7 @@ public class LangModel {
         private String baseUrl;
         private String apiKey;
         private String modelName;
+        private boolean stream;
 
         public Builder baseUrl(String baseUrl) {
             this.baseUrl = baseUrl;
@@ -66,6 +85,11 @@ public class LangModel {
 
         public Builder modelName(String modelName) {
             this.modelName = modelName;
+            return this;
+        }
+
+        public Builder stream(boolean stream) {
+            this.stream = stream;
             return this;
         }
 
