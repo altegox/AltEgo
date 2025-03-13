f5ccf0be34257efc55b9a81a14a1b8c4679f7750
citmina <gemingjia0201@163.com>
2025-03-13 11:47:52 +0800

    feat: add @Agent Annotation

---
+++ altego-framework/src/main/java/org/altegox/framework/annotation/Agent.java
@@ -0,0 +1,18 @@
+package org.altegox.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Agent {
+
+    String name();
+
+    String description() default "";
+
+    String[] groups() default {"default"};
+
+}
